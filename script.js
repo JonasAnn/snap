f08b4a0ac6365a3e6b90329a0aaa69539e1c6215
Jonah Ifeoma <ifyajoke@gmail.com>
2022-08-14 13:42:07 -0700

    added mouseover function

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+    document.getElementById("myDropdown-feature").classList.toggle("flex");
+   
+  }
+  function secondFunction() {
+    document.getElementById("myDropdown-feature-2").classList.toggle("flex");
+   
+  }
